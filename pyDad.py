f37554c27cd66886d1ee56527670487d21576243
scott <scottshuffler@gmail.com>
2015-07-03 10:34:44 -0400

    Basic recipe from picamera

--- pyDad.py
+++ pyDad.py
@@ -8,3 +8,13 @@ __author__ = 'Scott'
 #NEVER STOP
 
 #GOOD
+
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(2)
+    for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+        print('Captured %s' % filename)
+        time.sleep(300) # wait 5 minutes
