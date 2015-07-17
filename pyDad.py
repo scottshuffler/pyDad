e29c96d41caecd3d338a3da997a3eb491aba3e31
scott shuffler <scottshuffler@gmail.com>
2015-07-17 14:38:18 +0000

    Uploads and deletes

--- pyDad.py
+++ pyDad.py
@@ -8,13 +8,27 @@ __author__ = 'Scott'
 #NEVER STOP
 
 #GOOD
-
+import os
 import time
 import picamera
 
+from ftplib import FTP as ftp
+
 with picamera.PiCamera() as camera:
     camera.start_preview()
+    camera.hflip = True
+    camera.vflip = True
+    camera.resolution = (2592, 1944)
     time.sleep(2)
-    for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+    for filename in camera.capture_continuous('{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
         print('Captured %s' % filename)
+	session = ftp()
+	session.connect("ftp.oureportfolio.com",21)
+	session.login("username","password")
+	session.cwd("public_html/timelapse/")
+	session.storbinary('STOR ' + filename, open(filename, 'rb'))
+	print('Uploaded ' + filename)
+	session.close()
+	os.system('rm ' + filename)
+	print('Removing ' + filename)
         time.sleep(300) # wait 5 minutes
